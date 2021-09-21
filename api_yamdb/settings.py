62ac62d366c5fb4a121fbb57eac48241bfdfdad0
Vladislav <v.butirsky@gmail.com>
2021-09-21 17:20:23 +0700

    Check v1.5

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -99,9 +99,6 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        # 'TEST': {
-        #    'NAME': 'test_db',
-        # }
     }
 }
 
