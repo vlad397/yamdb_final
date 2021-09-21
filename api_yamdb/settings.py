e61c02694a7139bb14d72b1e950e41de15375380
Vladislav <v.butirsky@gmail.com>
2021-09-21 10:54:49 +0700

    Check v1.5

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
